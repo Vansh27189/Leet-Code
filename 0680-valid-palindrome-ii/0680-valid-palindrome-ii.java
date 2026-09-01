655c4b3b267b36cad91a6af79cccb4e73da52a11
Vansh <131522589+Vansh27189@users.noreply.github.com>
2026-09-01 20:53:08 +0530

    Time: 4 ms (99.07%), Space: 47.7 MB (68.89%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,35 +1,40 @@
 class Solution {
-    public boolean palindromehelper(int i, int j, String s){
+    public boolean ispalindrome(int i,int j, String s){
+        
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)){
                 return false;
-            }       
-            i = i+1;
-            j = j-1;
-
-        }   
-        return true; 
+            }
+            i++;
+            j--;
+            
+        }
+        return true;
+        
 
     }
+
+    
     public boolean validPalindrome(String s) {
         int i = 0;
-        int j = s.length() - 1;
+        int j = s.length()-1;
 
         while(i<j){
             char left = s.charAt(i);
             char right = s.charAt(j);
 
-            if(left != right){
-                //superpower 
-
-                return palindromehelper(i+1,j,s) || palindromehelper(i,j-1,s);
-                 
+            if(left!=right){
+                return ispalindrome(i+1,j,s) || ispalindrome(i,j-1,s);
             }
             else{
-                i = i + 1;
-                j = j - 1;
+                i++;
+                j--;
             }
         }
         return true;
+
+
+
+        
     }
 }
