52d3879d89126f338d3154b97d8e5617e35e7423
Vansh <131522589+Vansh27189@users.noreply.github.com>
2026-09-04 15:31:00 +0530

    Time: 27 ms (98.79%), Space: 59.1 MB (74.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,50 +1,48 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+    public void twosumhelper(int f,int[] nums,List<List<Integer>> res){
+        int i = f + 1;
+        int j = nums.length - 1;
 
-        Arrays.sort(nums);
+        while(i<j){
+            int sum = nums[f] + nums[i] + nums[j];
+            if(sum>0){
+                j--;
+            }
+            else if(sum<0){
+                i++;
+            }
+            else{
+                res.add(Arrays.asList(nums[i],nums[j],nums[f]));
+            
+                i++;
+                j--;
 
-        int n = nums.length;
+                while(i<j && nums[i]==nums[i-1]){
+                    i++;
 
-        for (int i = 0; i < n - 2; i++) {
-
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
-                    ans.add(Arrays.asList(
-                            nums[i],
-                            nums[left],
-                            nums[right]
-                    ));
-
-                    while (left < right &&
-                           nums[left] == nums[left + 1])
-                        left++;
-
-                    while (left < right &&
-                           nums[right] == nums[right - 1])
-                        right--;
-
-                    left++;
-                    right--;
-
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+                }
+                while(i<j && nums[j]==nums[j+1]){
+                    j--;
                 }
             }
-        }
 
-        return ans;
+        }
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int f = 0;f<nums.length;f++){
+            if(nums[f]>0){
+                break;
+            }
+
+            if(f==0 || nums[f]!=nums[f-1]){
+                twosumhelper(f,nums,res);
+
+            }
+        }
+        return res;
+        
     }
 }
